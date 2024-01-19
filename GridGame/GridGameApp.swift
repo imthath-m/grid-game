48c60fefeade5c06c94c3d0f22376e2d8075b83e
Mohammed Imthathullah <imthath.m@hotmail.com>
2024-01-19 20:00:11 +0530

    Initial Commit

---
+++ GridGame/GridGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  GridGameApp.swift
+//  GridGame
+//
+//  Created by Imthathullah on 19/01/24.
+//
+
+import SwiftUI
+
+@main
+struct GridGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
